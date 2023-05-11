c6d756fc06b55dedf0500f2a6228446f07613eef
mnlazs <mnl.azs@gmail.com>
2023-05-11 22:58:52 +0000

    task101

--- ES6_basic/101-iterateThroughObject.js
+++ ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,8 @@
 export default function iterateThroughObject(reportWithIterator) {
-	const employees = [];
-	for (const r of reportWithIterator) {
-		employees.push(r);
-	}
+ const employees = [];
+  for (const r of reportWithIterator) {
+    employees.push(r);
+ }
 
-	return employees.join(' | ');
+  return employees.join(' | ');
 }
