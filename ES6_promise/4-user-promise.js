625ae407d49bbff78a0bb2bd3272d74efb9fe976
mnlazs <mnl.azs@gmail.com>
2023-05-12 18:11:35 +0000

    updatrd

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -2,7 +2,7 @@ export default function signUpUser(firstName, lastName) {
   const myPromise = new Promise((resolve) => {
     resolve(
       { firstName, lastName },
-    )
+    );
   });
   return myPromise;
 }
