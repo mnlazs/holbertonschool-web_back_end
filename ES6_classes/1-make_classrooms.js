deb4192be609765783b874a5912a9e8847b6bea3
mnlazs <mnl.azs@gmail.com>
2023-05-16 21:13:50 +0000

    updated

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,16 +1,5 @@
-import Classroom from 0-classroom.js
-function initializeRooms() {
-    const sizes = [19, 20, 34];
-    const rooms = [];
-  
-    for (const size of sizes) {
-      const room = new ClassRoom(size);
-      rooms.push(room);
-    }
-  
-    return rooms;
-  }
-  
-  // Prueba la función
-  const roomsArray = initializeRooms();
-  console.log(roomsArray);
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
