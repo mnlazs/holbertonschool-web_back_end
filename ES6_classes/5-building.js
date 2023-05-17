6bf8632e1f1920041673c17957a6df8a0fb10202
mnlazs <mnl.azs@gmail.com>
2023-05-17 18:01:03 +0000

    updated

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,15 +3,15 @@ export default class Building {
     // Any class extending Building must implement below method
     if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
 
-// Create objs
+    // Create objs
     this._sqft = sqft;
   }
 
-// Methods
+  // Methods
 
-// Setters
+  // Setters
 
-// Getters
+  // Getters
   get sqft() {
     return this._sqft;
   }
