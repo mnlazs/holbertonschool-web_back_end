6a02dbf4bcc3d57e6d7cc9a07656128d500704cd
mnlazs <mnl.azs@gmail.com>
2023-05-17 18:23:07 +0000

    updated

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building.js'
+
+export default class SkyHighBuilding extends Building{
+  constructor(sqtf, floors){
+    super(sqtf);
+    this._floor = floors;
+  }
+
+//Methods
+evacuationWarningMessage(){
+  return 'Evacute slowly the ${this.floor} floors';
+  }
+
+//Setters
+
+//Getters
+get sqtf() {
+    return this._sqft;
+  }
+get floor() {
+    return this._floor;
+  }
+}
