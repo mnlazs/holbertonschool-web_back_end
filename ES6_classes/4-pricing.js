0e24a2481cb08f55c4f14c0ff066277e256874cc
mnlazs <mnl.azs@gmail.com>
2023-05-17 16:06:01 +0000

    updated

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency.js';
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and Setter for the amount attribute
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // Getter and Setter for the currency attribute
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Method to display the price in the desired format
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert the price using a conversion rate
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
