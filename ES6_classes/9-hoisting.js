838e596ae01390b6cabb151e194bf772ed6e63bb
mnlazs <mnl.azs@gmail.com>
2023-05-17 22:22:50 +0000

    updated

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -30,11 +30,7 @@ export default class HolbertonClass {
     get holbertonClass() {
       return this._holbertonClass;
     }
-  
-    get fullStudentDescription() {
-      return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-    }
-  }
+}
   
   const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
   const student2 = new StudentHolberton('John', 'Doe', class2020);
