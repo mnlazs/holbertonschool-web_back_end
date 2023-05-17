e8fff0386664aa8a3711508c2584e45a26d61b29
mnlazs <mnl.azs@gmail.com>
2023-05-17 22:25:59 +0000

    updated

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,4 +1,5 @@
-export default class HolbertonClass {
+/* eslint-disable */
+export class HolbertonClass {
     constructor(year, location) {
       this._year = year;
       this._location = location;
@@ -12,10 +13,10 @@ export default class HolbertonClass {
       return this._location;
     }
   }
-
+  
   const class2019 = new HolbertonClass(2019, 'San Francisco');
   const class2020 = new HolbertonClass(2020, 'San Francisco');
-
+  
   export class StudentHolberton {
     constructor(firstName, lastName, holbertonClass) {
       this._firstName = firstName;
@@ -30,7 +31,7 @@ export default class HolbertonClass {
     get holbertonClass() {
       return this._holbertonClass;
     }
-}
+  }
   
   const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
   const student2 = new StudentHolberton('John', 'Doe', class2020);
@@ -38,5 +39,6 @@ export default class HolbertonClass {
   const student4 = new StudentHolberton('Donald', 'Bush', class2019);
   const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
   
-  export const listOfStudents = [student1, student2, student3, student4, student5];
+  const listOfStudents = [student1, student2, student3, student4, student5];
   
+  export default listOfStudents;
