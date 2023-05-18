e89d5fe6b1acb28eed4e094e582f1c92faaf3e8b
mnlazs <mnl.azs@gmail.com>
2023-05-18 22:04:24 +0000

    updated

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -4,9 +4,3 @@ export default function groceriesList() {
   for (const i of arr) grocery.set(i[0], i[1]);
   return grocery;
 }
-export default function groceriesList() {
-  const arr = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
-  const grocery = new Map();
-  for (const i of arr) grocery.set(i[0], i[1]);
-  return grocery;
-}
