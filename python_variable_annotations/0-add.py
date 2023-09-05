3ed50e7a2719ca0655da9d9e0f1c66f00dfd37d3
mnlazs <mnl.azs@gmail.com>
2023-09-05 23:12:51 +0000

    updated

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Basic annotations - add """
+
+
 def add(a: float, b: float) -> float:
     """
     Suma dos números y devuelve el resultado.
     :return: La suma de a y b.
     """
     return a + b
-
