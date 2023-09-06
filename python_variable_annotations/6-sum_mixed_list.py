511800854405d86706eeeec24e539b26815e125d
mnlazs <mnl.azs@gmail.com>
2023-09-06 22:43:17 +0000

    updated

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Complex types - list of floats """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int,float]]) -> float:
+    """
+    Sum lista de números flot y return la sum número flotante.
+
+    :param input_list: Una lista de números flotantes.
+    :type input_list: List[float]
+
+    :return: La suma de los números flotantes en la lista.
+    :rtype: float
+"""
+    return sum(mxd_list)
