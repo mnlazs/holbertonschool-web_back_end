ff6d9f707040ca902bc902c62237b1969b1707bd
mnlazs <mnl.azs@gmail.com>
2023-09-06 22:17:20 +0000

    updated

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def sum_list(imput_list: float) -> float:
+    """
+    Suma una lista de números flotantes y devuelve la suma como un número flotante.
+    
+    :param input_list: Una lista de números flotantes.
+    :type input_list: List[float]
+    
+    :return: La suma de los números flotantes en la lista.
+    :rtype: float
+    """
+    return sum(imput_list)
