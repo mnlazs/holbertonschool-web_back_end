f402a6c158d5472b127199f6a57680b876e94f41
mnlazs <mnl.azs@gmail.com>
2023-09-06 22:23:40 +0000

    updated

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
+""" Complex types - list of floats """
 from typing import List
 
+""" Complex types - list of floats """
 def sum_list(imput_list: List[float]) -> float:
     """
-    Suma una lista de números flotantes y devuelve la suma como un número flotante.
-    
+    Sum lista de números flot y return la sum número flotante.
+
     :param input_list: Una lista de números flotantes.
     :type input_list: List[float]
-    
+
     :return: La suma de los números flotantes en la lista.
     :rtype: float
-    """
+"""
     return sum(imput_list)
