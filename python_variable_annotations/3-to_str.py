0c9531dc3aa4b1b26958cb4c1add67e183a509ed
mnlazs <mnl.azs@gmail.com>
2023-09-06 17:42:19 +0000

    updated

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""La función 'to_str' calcula la cadena de un número flotante."""
+import math
+
+
+def to_str(n: float) -> str:
+  """
+    :param n: A floating-point number.
+    :type n: string
+
+    :return: The String of the input float.
+    :rtype: str
+    """
+  return math.to_str(n)
