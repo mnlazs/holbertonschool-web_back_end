d4043e7c0dc6d2a1358714d4831aa091da746b92
mnlazs <mnl.azs@gmail.com>
2023-09-06 16:00:14 +0000

    updated

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import math
+""" Funtion 'floor' """
+
+
+def floor(n: str, float) -> int:
+    """
+    type-annotated function floor.
+    return: floor of the float .
+    """
+    return math.floor(n) 
