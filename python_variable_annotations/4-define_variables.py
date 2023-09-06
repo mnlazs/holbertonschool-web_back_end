5a5b40b7c6a9ea0dd49c87c84f5c98f9d21ea07c
mnlazs <mnl.azs@gmail.com>
2023-09-06 18:12:41 +0000

    updated

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-# Def de una variable 'a' de tipo entero (int) con el valor 1.
+# variable 'a' de tipo entero (int) con el valor 1.
 a: int = 1
 
-# Def de variable 'pi' de tipo punto flotante (float) con el valor 3.14.
+# variable 'pi' de tipo punto flotante (float) con el valor 3.14.
 pi: float = 3.14
 
-# Def de variable 'i_understand_annotations' de tipo booleano (bool) con el valor True.
+# var 'i_understand_annotations' de tipo booleano (bool) con el valor True.
 i_understand_annotations: bool = True
 
-# Def de una variable 'school' de tipo cadena de texto (str) con el valor "Holberton".
+# Def var 'school' de tipo cadena de texto (str) con el valor "Holberton".
 school: str = "Holberton"
