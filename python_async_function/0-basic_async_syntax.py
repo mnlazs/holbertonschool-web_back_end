e7a2b2dae58185b4e8c4c4e865f627eacf7cd5b4
mnlazs <mnl.azs@gmail.com>
2023-09-07 18:57:26 +0000

    updated

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -9,6 +9,6 @@ async def wait_random(max_delay: int = 10) -> float:
         (max_delay, with a default value of 10) named wait_random that waits
         for a random delay between 0 and max_delay (included and float value)
         seconds and eventually returns it.  """
-    random_float = random.uniform(0, max_delay)
+    random_espera = random.uniform(0, max_delay)
     await asyncio.sleep(random_float)
-    return random_float
+    return random_espera
