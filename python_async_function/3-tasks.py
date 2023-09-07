cb4a1bd854eb146140694f5bc5b9299ebc293a16
mnlazs <mnl.azs@gmail.com>
2023-09-07 20:06:08 +0000

    updated

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
-"""Import wait_random from the previous python file"""
+""" Import wait_random from 0-basic_async_syntax."""
 import asyncio
-from typing import Any
-from random import uniform
-from asyncio import Task
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_random(max_delay: int = 10) -> float:
-    """Función asincrónica que espera un tiempo aleatorio entre 0 y max_delay."""
-    random_float = uniform(0, max_delay)
-    await asyncio.sleep(random_float)
-    return random_float
 
-def task_wait_random(max_delay: int) -> Task[Any]:
-    """Función que crea una tarea asincrónica que ejecuta wait_random(max_delay)."""
-    return asyncio.create_task(wait_random(max_delay))
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ Tasks """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
