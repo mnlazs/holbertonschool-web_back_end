472705b3b28aa4e141b67f17b40511fc9797d2b6
mnlazs <mnl.azs@gmail.com>
2023-09-07 14:45:52 +0000

    updated

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -3,19 +3,19 @@
 from typing import Callable
 
 
-def make_multiplier (multiplier: float) -> Callable[[float], float]:
-  """
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
     Toma un multiplicador flotante como argumento y devuelve una función que
     multiplica un número flotante por el multiplicador.
 
-    :param multiplier: Un número flotante que se utilizará para la multiplicación.
+    :param multiplier: Numero flotante q se utilizará para la multiplicación.
     :type multiplier: float
 
     :return: Una función que toma un número flotante y devuelve el resultado de
     multiplicar ese número por el multiplicador.
     :rtype: Callable[[float], float]
     """
-  def multiplier_function(x: float) -> float:
+    def multiplier_function(x: float) -> float:
         return x * multiplier
 
-  return multiplier_function
+    return multiplier_function
