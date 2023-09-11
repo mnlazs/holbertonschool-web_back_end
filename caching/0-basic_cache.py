1d7b2e7ba12796d200b5037b05a7641ac3b34ba8
mnlazs <mnl.azs@gmail.com>
2023-09-11 23:12:56 +0000

    updated

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -4,6 +4,7 @@
 from base_caching import BaseCaching
 
 # Definicion de la super clase
+
 class BasicCache(BaseCaching):
     """ Class that inherits from BaseCaching and is a caching system
         This caching system doesn’t have limit """
@@ -11,15 +12,15 @@ class BasicCache(BaseCaching):
         """ Initiliaze
         """
         super().__init__()
-        
+
     def put(self, key, item):
-      """El método put representan la clave y el valor 
+      """El método put representan la clave y el valor
           que se agregarán a la caché."""
-      if key is not None and item is not None:
-        self.cache_data[key] = item
-          
+        if key is not None and item is not None:
+          self.cache_data[key] = item
+
     def get(self, key):
-          """ Return the value """
-          if key is None or key not in self.cache_data:
-            return None
-          return self.cache_data[key]
+    """ Return the value """
+      if key is None or key not in self.cache_data:
+        return None
+        return self.cache_data[key]
