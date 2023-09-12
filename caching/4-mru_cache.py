5c2bb01d4f97bec9c4160c8dbebcd373ba740e92
mnlazs <mnl.azs@gmail.com>
2023-09-12 19:48:04 +0000

    updated

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -5,6 +5,7 @@ from base_caching import BaseCaching
 
 class MRUCache(BaseCaching):
     """ Class that inherits from BaseCaching and is a caching system """
+
     def __init__(self):
         super().__init__()
         self.head, self.tail = 'head', 'tail'
