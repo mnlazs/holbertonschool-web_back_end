40971693a985304936bb06b5f895f27de48fca85
mnlazs <mnl.azs@gmail.com>
2023-09-13 16:46:42 +0000

    updated

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""funcion index_range"""
+
 
 def index_range(page, page_size):
-    
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size - 1
-    
-    return (start_index, end_index)
+    """return una tupla"""
+    if page and page_size:
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return (start_index, end_index)
