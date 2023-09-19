8415878ac5956623704e76430b5189233e6af53d
mnlazs <mnl.azs@gmail.com>
2023-09-19 18:45:30 +0000

    updated

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -10,4 +10,4 @@ def hash_password(password: str) -> bytes:
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ recibe dos argumentos y retorna un boolen"""
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password())
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
