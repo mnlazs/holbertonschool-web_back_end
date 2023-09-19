722fd551ebc24aa83de3d6935b6aa77cdd5e8b51
mnlazs <mnl.azs@gmail.com>
2023-09-19 18:38:24 +0000

    updated

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -4,5 +4,5 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-
+    """funcion que recibe un argumento password y retorna salted"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
