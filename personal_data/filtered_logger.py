b76d9cc4c31c9e83802ff77c31fd75c46ee62873
mnlazs <mnl.azs@gmail.com>
2023-09-19 17:29:23 +0000

    updated

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,5 +5,6 @@ from typing import List
 Funtion filter_datum
 """
 
+
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     return re.sub(r'(' + '|'.join(fields) + r')=[^' + separator + r']*', f'\\1={redaction}', message)
