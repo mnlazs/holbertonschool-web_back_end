6cba18ad7f2b4a7968496e736230bd57a22f7c44
mnlazs <mnl.azs@gmail.com>
2023-09-19 18:05:33 +0000

    updated

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,9 +4,7 @@ Tasks 0. Regex-ing
 """
 import re
 from typing import List
-"""
-Funtion filter_datum
-"""
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -16,3 +14,19 @@ def filter_datum(fields: List[str],
     "retorna el mensaje sofuscado"
     return re.sub(r'(' + '|'.join(fields) + r')=[^' + separator + r']*',
                   f'\\1={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filtra los valores de los registros"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
