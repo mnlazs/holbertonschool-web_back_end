8283f735a7e56107b55b7ea39ec5660a54a4fc83
mnlazs <mnl.azs@gmail.com>
2023-09-20 23:45:02 +0000

    updated

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized():
+    # Aquí provocamos el error 401
+    abort(401)
