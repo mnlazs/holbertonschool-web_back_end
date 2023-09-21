4fbd284d861f19a1766429b265dfa8a5895ef748
mnlazs <mnl.azs@gmail.com>
2023-09-21 17:01:55 +0000

    fixing pycodestyle issue

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -6,9 +6,10 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """Auth class to manage the API authentication"""
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method for validating if the path requires authentication"""
         return False
