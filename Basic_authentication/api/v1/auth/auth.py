caa639ee05b313265a76f22a389c58fd7a490a60
mnlazs <mnl.azs@gmail.com>
2023-09-21 18:18:36 +0000

    pep8

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -6,11 +6,12 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """Auth class to manage the API authentication"""
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Metodo para validar que el path no requiere validacion"""
+        """Metodo para validar que el path no requiere valida"""
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
         if path[-1] != '/':
