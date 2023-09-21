c564d2d75deddd0489aef5c74954680a0561ecad
mnlazs <mnl.azs@gmail.com>
2023-09-21 18:15:21 +0000

    updated-aut

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -6,13 +6,18 @@
 from flask import request
 from typing import List, TypeVar
 
-
 class Auth:
     """Auth class to manage the API authentication"""
-
+    
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Method for validating if the path requires authentication"""
-        return False
+        """Metodo para validar que el path no requiere validacion"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
