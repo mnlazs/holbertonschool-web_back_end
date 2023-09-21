50d2f3e7c2878e86cd4187824fee1f2bcc4befc9
mnlazs <mnl.azs@gmail.com>
2023-09-21 15:45:45 +0000

    pycodestyl

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -13,11 +13,13 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 @app.errorhandler(401)
 def unauthorized_error(error):
     """unauthorizacion creada por la funcion.
     """
     return jsonify({"error": "Unauthorized"}), 401
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
