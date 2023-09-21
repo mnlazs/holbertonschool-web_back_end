6b7ef06b483ec6bae3a804eec7e98e2fed8f9b04
mnlazs <mnl.azs@gmail.com>
2023-09-21 17:00:21 +0000

    auth-file

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
+"""class to manage the API authentication.
+    Returns:
+        none
+    """
 from flask import request
 from typing import List, TypeVar
 
 class Auth:
+    """Auth class to manage the API authentication"""
+    
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Method for validating if the path requires authentication"""
         return False
 
     def authorization_header(self, request=None) -> str:
+        """
+        Method for validating if the header contains the Authorization
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Method for getting the current user
+        """
         return None
