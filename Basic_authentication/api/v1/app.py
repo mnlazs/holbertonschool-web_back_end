31bfe63b413d101de378a2dedf97a43e2d2dafbe
mnlazs <mnl.azs@gmail.com>
2023-09-22 14:21:49 +0000

    updated

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,13 +12,33 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+if getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
-@app.errorhandler(401)
-def unauthorized_error(error):
-    """unauthorizacion creada por la funcion.
+@app.before_request
+def before_request_func():
+    """ Before request handler
     """
-    return jsonify({"error": "Unauthorized"}), 401
+    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+             '/api/v1/status']
+
+    if auth is None:
+        return
+
+    if not auth.require_auth(request.path, paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
@@ -28,9 +48,17 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden error handled"""
+    """ forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -24,7 +24,9 @@ class Auth:
         """
         Method for validating if the header contains the Authorization
         """
-        return None
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
