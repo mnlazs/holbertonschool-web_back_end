80bc7537700729943d5f467e8e4ee3ab64301789
mnlazs <mnl.azs@gmail.com>
2023-09-25 19:16:32 +0000

    updated

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -7,12 +7,13 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+from api.v1.auth.session_auth import SessionAuth  # Importa SessionAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
+
+# Actualiza la variable 'auth' según el valor de 'AUTH_TYPE'
 AUTH_TYPE = getenv("AUTH_TYPE")
 if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
@@ -21,8 +22,7 @@ elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 elif AUTH_TYPE == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
+    auth = SessionAuth()  # Crea una instancia de SessionAuth y asígnala a 'auth'
 
 
 @app.errorhandler(401)
