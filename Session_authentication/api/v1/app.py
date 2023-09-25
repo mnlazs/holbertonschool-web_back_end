2ca820bc3a6c44a790cedb03b5ebd77f5969dddc
mnlazs <mnl.azs@gmail.com>
2023-09-25 19:04:04 +0000

    updated

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -55,8 +55,9 @@ def before_request() -> str:
                       '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
-      abort(401)
+    if not auth.authorization_header(request)\
+    and not auth.session_cookie(request):
+        abort(401)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
