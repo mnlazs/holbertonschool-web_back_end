d278f70dd9d6735b93e495d9c2e22987e32ae2d8
mnlazs <mnl.azs@gmail.com>
2023-09-26 22:02:44 +0000

    updated

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -2,7 +2,6 @@
 """creando a SQLAlchemy model named User"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
 Base = declarative_base()
 
 
@@ -11,10 +10,10 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=False)
-    reset_token = Column(String, nullable=False)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
 
     def __repr__(self):
         return "<User(email='%s', hashed_password='%s', session_id='%s',\
