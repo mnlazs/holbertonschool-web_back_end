35c36dbf0733db9907b79266cee4075368119080
mnlazs <mnl.azs@gmail.com>
2023-09-26 21:50:34 +0000

    updated

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -7,15 +7,17 @@ Base = declarative_base()
 
 
 class User(Base):
-  """Tabla Base"""
-  __tablename__ = 'users'
-  
-  id = Column(Integer, primary_key=True)
-  email = Column(String, nullable=False)
-  hashed_password = Column(String, nullable=False)
-  session_id = Column(String, nullable=False)
-  reset_token = Column(String, nullable=False)
+    """Tabla Base"""
+    __tablename__ = 'users'
 
-  def __repr__(self):
-      return "<User(email='%s', hashed_password='%s', session_id='%s', reset_token='%s')>" % (
-                        self.email, self.hashed_password, self.session_id, self.session_id, self.reset_token)
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=False)
+    reset_token = Column(String, nullable=False)
+
+    def __repr__(self):
+        return "<User(email='%s', hashed_password='%s', session_id='%s',\
+        reset_token='%s')>" % (
+            self.email, self.hashed_password, self.session_id,
+            self.session_id, self.reset_token)
