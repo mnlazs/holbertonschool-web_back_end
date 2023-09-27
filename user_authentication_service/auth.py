378a773a57a903b4463aadefa8a15d69b39efd29
mnlazs <mnl.azs@gmail.com>
2023-09-27 19:40:18 +0000

    updated

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
 from typing import Union
 
+
 def _hash_password(password: str) -> str:
     """ Returns a salted hash of the input password """
     return hashpw(password.encode('utf-8'), gensalt())
