49344f837d9faf293df5453cc87da19f86d0b4f1
mnlazs <mnl.azs@gmail.com>
2023-09-27 20:19:18 +0000

    updated

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -6,10 +6,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def welcome():
     message = {"message": "Bienvenue"}
     return jsonify(message)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
