37bcca1790e51338d33316f5b36332947f38ceae
mnlazs <mnl.azs@gmail.com>
2023-09-27 19:57:29 +0000

    updated

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -10,3 +10,23 @@ from typing import Union
 def _hash_password(password: str) -> str:
     """ Returns a salted hash of the input password """
     return hashpw(password.encode('utf-8'), gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Registers and returns a new user if email isn't listed"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+
+            return user
+
+        else:
+            raise ValueError(f'User {email} already exists')
