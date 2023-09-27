b4dadb4eb209bdc27cb3aa020240963b47db87e1
mnlazs <mnl.azs@gmail.com>
2023-09-27 21:11:11 +0000

    updated

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -32,6 +32,31 @@ def register_user() -> str:
     msg = {"email": email, "message": "user created"}
     return jsonify(msg)
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ POST /sessions"""
+    form_data = request.form
+
+    if "email" not in form_data:
+        return jsonify({"message": "email required"}), 400
+    elif "password" not in form_data:
+        return jsonify({"message": "password required"}), 400
+    else:
+
+        email = request.form.get("email")
+        pswd = request.form.get("password")
+
+        if AUTH.valid_login(email, pswd) is False:
+            abort(401)
+        else:
+            session_id = AUTH.create_session(email)
+            response = jsonify({
+                "email": email,
+                "message": "logged in"
+                })
+            response.set_cookie('session_id', session_id)
+
+            return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
