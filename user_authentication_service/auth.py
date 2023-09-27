96ba65647366deccf9a1536db81c552639f266dc
mnlazs <mnl.azs@gmail.com>
2023-09-27 20:06:40 +0000

    updated

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -11,6 +11,7 @@ def _hash_password(password: str) -> str:
     """ Returns a salted hash of the input password """
     return hashpw(password.encode('utf-8'), gensalt())
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
