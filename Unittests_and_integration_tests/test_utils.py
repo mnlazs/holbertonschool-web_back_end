46509fed6e492476960bc37752403e6a6bfb5f4f
mnlazs <mnl.azs@gmail.com>
2023-09-30 16:30:27 +0000

    updated

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -28,39 +29,6 @@ class TestAccessNestedMap(unittest.TestCase):
         # Verificando el mensaje de excepción
         actual_exception_message = str(context.exception)
         self.assertEqual(actual_exception_message, expected_exception_message)
-
-
-class TestGetJson(unittest.TestCase):
-    
-    @patch('utils.request.get')
-    def test_get_json(self, mock_get):
-        #definiendo los casos de prueba
-        test_cases = [
-            ("http://example.com", {"payload": True}),
-            ("http://holberton.io", {"payload": False})
-        ]
-        # Iterar a través de los casos de prueba
-        for test_url, test_payload in test_cases:
-            # Crear un objeto Mock para la respuesta
-            mock_response = Mock()
-            # Configurar el método json del objeto Mock para que devuelva el test_payload
-            mock_response.json.return_value = test_payload
-            # Configurar el valor de retorno del método get del objeto Mock
-            mock_get.return_value = mock_response
-
-            # Llamar a la función get_json con el test_url
-            resultado = get_json(test_url)
-
-            # Asegurar que el método get simulado se llamó exactamente una vez con test_url como argumento
-            mock_get.assert_called_once_with(test_url)
-
-            # Asegurar que el resultado de get_json es igual a test_payload
-            self.assertEqual(resultado, test_payload)
-
-    
-    
-    
-    
 
     if __name__ == '__main__':
         unittest.main()
