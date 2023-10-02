caf88ff30de5736298313d5c4aca120cc8f9b37c
mnlazs <mnl.azs@gmail.com>
2023-10-02 20:00:13 +0000

    updated

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Casos de Prueba"""
+
+import requests
+import unittest
+from unittest.mock import patch, Mock, PropertyMock, call
+from parameterized import parameterized, parameterized_class
+import utils
+from utils import access_nested_map, get_json, memoize
+from client import GithubOrgClient
+import client
+from fixtures import TEST_PAYLOAD
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Test that json can be got """
+
+    @parameterized.expand([
+        ("google", {"google": True}),
+        ("abc", {"abc": True})
+    ])
+    @patch('client.get_json')
+    def test_org(self, org, expected, get_patch):
+        """ Test the org of the client """
+        get_patch.return_value = expected
+        x = GithubOrgClient(org)
+        self.assertEqual(x.org, expected)
+        get_patch.assert_called_once_with("https://api.github.com/orgs/"+org)
+
+    def test_public_repos_url(self):
+        """ test that _public_repos_url works """
+        expected = "www.yes.com"
+        payload = {"repos_url": expected}
+        to_mock = 'client.GithubOrgClient.org'
+        with patch(to_mock, PropertyMock(return_value=payload)):
+            cli = GithubOrgClient("x")
+            self.assertEqual(cli._public_repos_url, expected)
