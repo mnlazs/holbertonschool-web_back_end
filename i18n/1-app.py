7d65d90ab591423d3f589935e5cdd4cea47bedb7
mnlazs <mnl.azs@gmail.com>
2023-10-03 19:34:45 +0000

    pycodestyle issues

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -4,17 +4,20 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
-app = Flask(__name__) #creando la instancia de la app Flask
-babel = Babel(app) #configurando la instacia babel
+app = Flask(__name__)  # creando la instancia de la app Flask
+babel = Babel(app)  # configurando la instacia babel
 
 
-class Config: #creando la clase con los idiomas disponibles
-  """descripcion de la clase"""
-  LANGUAGES = ["en", "fr"]
-  BABEL_DEFAULT_LOCALE = "en"
-  BABEL_DEFAULT_TIMEZONE = "UTC"
+class Config:  # creando la clase con los idiomas disponibles
+    """descripcion de la clase"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
-app.config.from_object(Config) #Configurar la aplicación Flask con la clase de configuración
+
+# Configurar la aplicación Flask con la clase de configuración
+app.config.from_object(Config)
+
 
 @app.route("/")
 def index():
