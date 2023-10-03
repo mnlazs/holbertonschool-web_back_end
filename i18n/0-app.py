ba694a76ef414b13ddf6d164195726757ba1c7f7
mnlazs <mnl.azs@gmail.com>
2023-10-03 18:26:43 +0000

    update

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+"""Flask Task
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_word():
+  """template"""
   return 
+
+if __name__ == "__main__":
+    app.run()
