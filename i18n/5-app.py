fe2e218b238001e37d869acd071100afb2111145
mnlazs <mnl.azs@gmail.com>
2023-10-05 18:25:23 +0000

    updated

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""Mocking"""
-
-from flask import Flask, g, request, render_template
+""" Task 5"""
 from flask_babel import Babel
-from os import getenv
-from typing import Union
+from flask import Flask, render_template, request, g
+
+app = Flask(__name__)
+babel = Babel(app)
+
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -13,12 +14,9 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
-app = Flask(__name__)
-babel = Babel(app)
-
 
 class Config:
-    """estableciendo la clase Babel"""
+    """ cconfiguracion del lenguaje"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -29,13 +27,18 @@ app.config.from_object(Config)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """ first index template to getting started translate"""
+    """ first index template to getting started translate
+    Args:
+        template rendering
+    """
     return render_template('5-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """ definicion de la clase
+    """ GET localization of my user
+    Returns:
+        the best languages apply to his/her zone
     """
     locale = request.args.get('locale')
     if locale is None:
@@ -45,20 +48,24 @@ def get_locale():
         return locale
 
 
+def get_user():
+    """ Get user from mock db "users"
+    Returns:
+        [type]: [description]
+    """
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+
+
 @app.before_request
 def before_request():
-    """se ejecuta antes de cada solicitud"""
-    user_id = request.args.get("login_as")
-    g.user = get_user(int(user_id)) if user_id else None
-
-
-def get_user(user_id, login_as):
-    """solicita informacion sobre los usuarios"""
-    usuario = users.get(user_id)
-    if usuario and login_as:
-        return usuario
-    return None
+    """ logged in user if is paased like args
+    """
+    user = get_user()
+    if user:
+        g.user = user
 
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", 5000)
+    app.run('0.0.0.0', '5000')
