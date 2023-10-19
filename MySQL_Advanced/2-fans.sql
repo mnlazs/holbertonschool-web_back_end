6a0d7420573b23eb17b47144e57d27a5367820c4
mnlazs <mnl.azs@gmail.com>
2023-10-19 01:58:57 +0000

    updated

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,3 +1,5 @@
+-- Task : 0. We are all unique! - creates a table users
+-- script can be executed on any database
 SELECT DISTINT `origin`, SUM(`fans`) as `nb_fans` FROM `metal_bands`
 GROUP BY `origin`
 ORDER BY `nb_fans` DESC;
