e97f4b2444fc150bf75a9ea410a88da39400920e
mnlazs <mnl.azs@gmail.com>
2023-10-21 14:59:04 -0500

    updated

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,5 +1,19 @@
 // 1-stdin.js
-function displayMessage(message) {
-  console.log(message);
-}
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (name) => {
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
+  rl.close();
+});
+
+rl.on('close', () => {
+  process.exit(0);
+});
