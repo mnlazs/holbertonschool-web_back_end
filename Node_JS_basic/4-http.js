3cf7756193242cde0b39ba733205c2fe4585b151
mnlazs <mnl.azs@gmail.com>
2023-10-23 12:44:04 -0500

    updated

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,16 +1,11 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 1245;
-
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  res.end('Hello Holberton School!\n');
 });
 
-app.listen(port, hostname, () => {
-  //   console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(1245);
 
 module.exports = app;
