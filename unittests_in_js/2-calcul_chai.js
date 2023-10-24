d7544e7d1e3a69ce08e923a63fe02f3ede78f4e3
mnlazs <mnl.azs@gmail.com>
2023-10-24 17:35:42 -0500

    updated

--- unittests_in_js/2-calcul_chai.js
+++ unittests_in_js/2-calcul_chai.js
@@ -1,18 +1,7 @@
 'use strict';
-const calculateNumber = (type, a, b) => {
-  switch (type) {
-    case 'SUM':
-      return Math.round(a) + Math.round(b);
-      break;
-    case 'SUBTRACT':
-      return Math.round(a) - Math.round(b);
-      break;
-    case 'DIVIDE':
-      if (Math.round(b) === 0) return 'Error';
-      return Math.round(a) / Math.round(b);
-      break;
-    default:
-      break;
-  };
+function calculateNumber(type, a, b) {
+  if (type === 'SUM') return Math.round(a) + Math.round(b);
+  if (type === 'SUBTRACT') return Math.round(a) - Math.round(b);
+  if (type === 'DIVIDE') return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
 }
 module.exports = calculateNumber;
