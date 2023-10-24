b124a939ebbce37a6cb85367d93868237101cb8e
mnlazs <mnl.azs@gmail.com>
2023-10-24 13:20:26 -0500

    updated

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,9 +2,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
+  const tunombre = process.stdin.read();
+  if (tunombre !== null) {
+    process.stdout.write(`Your name is: ${tunombre}`);
   }
 });
 
